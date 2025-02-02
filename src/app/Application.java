e938145244c078773da5bbc01f6ae79d7e47ab67
Eduardo <Eduardo.daSilva@studentfr.ch>
2025-02-02 19:08:14 +0100

    devoir 1 finis

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,17 +1,26 @@
 package app;
+    
+import models.Ami;
 
 public class Application {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
-
+    public static void main(String[] args) {
+        Ami zbibou = new Ami("zbibou", 5);
+        Ami joachim = new Ami("joachim", 10);
+        Ami viego = new Ami("viego", 2);
+        do {
+            zbibou.getBiere();
+            zbibou.boitUneBiere();
+            joachim.getBiere();
+            joachim.boitUneBiere();
+            viego.getBiere();
+            viego.boitUneBiere();
+        } while (!viego.getEstBouree() && !joachim.getEstBouree() && !zbibou.getEstBouree());
+        if (viego.getEstBouree()) {
+            System.out.println("viego est bouré");
+        } else if (joachim.getEstBouree()) {
+            System.out.println("joachim est bourée");
+        } else {
+            System.out.println("zbibou est bourée");
+        }
+    }
 }
